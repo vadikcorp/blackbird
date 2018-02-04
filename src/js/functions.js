62444df92278e4638cd257a17bc7af8aad6668f2
vadikcorp <vadum.vasko@gmail.com>
2018-02-05 01:05:34 +0200

    initial

---
+++ src/js/functions.js
@@ -0,0 +1,17 @@
+$(window).scroll(function(){
+
+	var wScroll = $(this).scrollTop();
+
+	$('.logo').css({
+    	'transform' : 'translate(0px, '+ wScroll /2 +'%)'
+  	});
+
+	$('.back-bird').css({
+    	'transform' : 'translate(0px, '+ wScroll /4 +'%)'
+ 	});
+
+ 	$('.fore-bird').css({
+    	'transform' : 'translate(0px, -'+ wScroll /40 +'%)'
+  	});
+
+});
